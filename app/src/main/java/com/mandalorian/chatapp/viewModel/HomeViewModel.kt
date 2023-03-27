afa544ce23c869fccd51193a12d1fc74fc684fd7
juju <jcmc888@gmail.com>
2023-03-27 17:52:03 +0800

    Completed filter in home fragment

--- app/src/main/java/com/mandalorian/chatapp/viewModel/HomeViewModel.kt
+++ app/src/main/java/com/mandalorian/chatapp/viewModel/HomeViewModel.kt
@@ -3,22 +3,29 @@ package com.mandalorian.chatapp.viewModel
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.viewModelScope
 import com.mandalorian.chatapp.data.model.User
+import com.mandalorian.chatapp.data.service.AuthService
 import com.mandalorian.chatapp.repository.UserRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
-class HomeViewModel @Inject constructor(private val userRepository: UserRepository) :
+class HomeViewModel @Inject constructor(
+    private val userRepository: UserRepository,
+    private val authService: AuthService
+) :
     BaseViewModel() {
     val users: MutableLiveData<List<User>> = MutableLiveData()
 
     override fun onViewCreated() {
         super.onViewCreated()
         viewModelScope.launch {
+            val user = authService.getCurrentUser()
             val res = safeApiCall { userRepository.getUsers() }
-            res.let {
-                users.value = it
+            res?.let {
+                users.value = it.filterNot { person ->
+                    person.username == user?.username
+                }
             }
         }
     }
